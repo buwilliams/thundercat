e3f3c839a11f02b454eefbd258772333118b49d8
Buddy Williams <bwilliams@scoutahead.com>
2018-06-01 15:19:42 -0400

    Added user management

--- src/generate_password.js
+++ src/generate_password.js
@@ -1,10 +1,8 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
 
 var plainTextPassword = process.argv[2];
 
 bcrypt.hash(plainTextPassword, saltRounds, function(err, hash) {
-    console.log('Hash: ', hash);
+    console.log('Hash:', hash);
 });
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const _ = require('lodash');
 
 app.use(bodyParser.json());
@@ -27,13 +28,12 @@ var storage = readStorage();
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 function authorize(req, res, next) {
-    console.log('Authorize session', req.session.auth);
     var authorized = req.session.auth;
     if(authorized === true) {
-        console.log('Authorized request.');
+        console.log('Authorized request:', req.originalUrl, '<' + req.ip + '>');
         next();
     } else {
-        console.log('Unauthorized request.');
+        console.log('Unauthorized request:', req.originalUrl, '<' + req.ip + '>');
         res.redirect('/admin/auth/login');
     }
 }
@@ -86,9 +86,43 @@ app.get('/admin/auth/logout', (req, res) => {
     })
 });
 
-// TODO: list users
-// TODO: create user
-// TODO: delete user
+app.get('/admin/auth/users', [authorize], (req, res) => {
+    res.send(storage.users);
+});
+
+app.post('/admin/auth/users', [authorize], (req, res, next) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        storage.users.push({
+            email: email,
+            encryptedPassword: hash,
+            type: 'admin'
+        });
+
+        writeStorage(storage);
+
+        res.send(storage.users);
+        next();
+    });
+});
+
+app.delete('/admin/auth/users', [authorize], (req, res) => {
+    var email = req.body.email;
+
+    if(storage.users.length === 1) {
+        res.send(storage.users);
+        return;
+    }
+
+    var index = _.findIndex(storage.users, {email: email});
+    storage.users.splice(index, 1);
+    writeStorage(storage);
+
+    res.send(storage.users);
+});
+
 // TODO: change password
 
 app.listen(3000, () => {
